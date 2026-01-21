b9e1d009d63b4df610029a1439002cff11cda735
kumaravel4123 <kumaravel3124@gmail.com>
2026-01-21 12:55:18 +0530

    added fils

--- Eshop/products/urls.py
+++ Eshop/products/urls.py
@@ -2,8 +2,15 @@ from django.urls import path
 
 from .views import productView, searchProducts
 
+from .views import (
+    CreateProduct, ProductDetail, UpdateProduct, DeleteProduct
+)
+
 urlpatterns = [
-    # path('all/', productView, name = 'products'),
-    path('products/', productView, name= 'product_page'),
-    path('serach/', searchProducts, name= 'search_products')
+    path('all/', productView, name = 'product_page'),
+    path('serach/', searchProducts, name= 'search_products'),
+    path('add/', CreateProduct.as_view(), name='add_product'),
+    path('<int:pk>/', ProductDetail.as_view(), name='product_details'),
+    path('<int:pk>/edit/', UpdateProduct.as_view(), name='edit_product'),
+    path('<int:pk>/delete/', DeleteProduct.as_view(), name='delete_product')
 ]
--- Eshop/products/views.py
+++ Eshop/products/views.py
@@ -32,3 +32,32 @@ def searchProducts(request):
 
     return render(request, template_name=template, context= context)
 
+# CRUD Operations using Generic Class Based Views of Django
+
+from django.views.generic import ( CreateView, DetailView,
+                                   UpdateView, DeleteView )
+
+# ListView has already been implemented using a function above : productView()
+
+class CreateProduct(CreateView):
+    model = Product
+    template_name = 'products/add_product.html'
+    fields = '__all__'
+    # redirection url for successful creation of resource
+    success_url = '/'
+
+class ProductDetail(DetailView):
+    model = Product
+    template_name = 'products/product_details.html'
+
+class UpdateProduct(UpdateView):
+    model = Product
+    template_name = 'products/update_product.html'
+    fields = '__all__'
+    success_url = '/'
+
+class DeleteProduct(DeleteView):
+    model = Product
+    template_name = 'products/delete_product.html'
+    success_url = '/'
+
