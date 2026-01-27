c9014a87acc2d11b5c73fe2ebff9ccbe0046fce4
kumaravel4123 <kumaravel3124@gmail.com>
2026-01-27 20:23:01 +0530

    edits

--- Eshop/products/views.py
+++ Eshop/products/views.py
@@ -29,6 +29,11 @@ def searchProducts(request):
             'query' : query,
             'products' : search_results
         }
+    else:
+        context = {
+            'query' : query,
+            'products' : None
+        }
 
     return render(request, template_name=template, context= context)
 
