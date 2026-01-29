6ab3f496198ffddc9b44d1102be7a0ad4cb082a4
kumaravel4123 <kumaravel3124@gmail.com>
2026-01-29 22:27:50 +0530

    edits

--- Eshop/mainapp/urls.py
+++ Eshop/mainapp/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('', views.homeView, name='home_page'),
     path('about/', views.aboutView, name='about_page'),
     path('contact/', views.contactView, name='contact_page'),
-    # path('products/', views.productView, name='product_page')
 ]
