a4a027fa6ed2a54086880805c7e5d5e2c21f8274
kumaravel4123 <kumaravel3124@gmail.com>
2026-01-30 12:44:55 +0530

    added new app cart

--- Eshop/products/forms.py
+++ Eshop/products/forms.py
@@ -1,5 +1,22 @@
 from django import forms
-from .models import ProductImage
+from .models import ProductImage,Product
+
+
+BOOTSTRAP_ATTRS = {
+    'class' : 'form-control'
+}
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = [ 'title', 'desc', 'price', 'stock', 'thumbnail']
+        widgets = {
+            'title' : forms.TextInput(attrs=BOOTSTRAP_ATTRS),
+            'desc' : forms.Textarea(attrs=BOOTSTRAP_ATTRS),
+            'price' : forms.NumberInput(attrs=BOOTSTRAP_ATTRS),
+            'stock' : forms.NumberInput(attrs=BOOTSTRAP_ATTRS),
+            'thumbnail' : forms.ClearableFileInput(attrs=BOOTSTRAP_ATTRS)
+        }
 
 class ProductImageForm(forms.ModelForm):
     class Meta:
